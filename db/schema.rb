b04f5fdeba5fd2734f4249479caf945194e853f1
Chris Oliver <excid3@gmail.com>
2026-03-11 10:26:27 -0500

    Ruby 4.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_06_18_163423) do
+ActiveRecord::Schema[8.0].define(version: 2024_06_18_163423) do
   create_table "todos", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
